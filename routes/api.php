6744141130df337f0a8980a5d70944676c6f17f6
putrik44yl <kylarajagula444@example.com>
2026-04-08 14:09:53 +0700

    new

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-// Import controller yang benar
 use App\Http\Controllers\Api\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,7 +9,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    // Route test untuk cek apakah user sudah login
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
